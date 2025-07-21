e7811c131cc952769d65241997ba3ecc3f6cd0ee
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-21 15:49:05 +0530

    implemented verify otp usecase

---
+++ backend/src/useCases/auth/verifyOtp.ts
@@ -0,0 +1,23 @@
+import { IkeyValueTTLCaching } from "../../domain/interfaces/respository/cacheStorage/IKeyValueTTLCaching";
+import { IVerifyOtpUseCase } from "../../domain/interfaces/useCase/auth/IVerifyOtp";
+
+export class VerifyOtpUseCase implements IVerifyOtpUseCase {
+    private cacheStorage: IkeyValueTTLCaching;
+
+    constructor(cacheStorage: IkeyValueTTLCaching) {
+        this.cacheStorage = cacheStorage;
+    }
+
+    async verifyOtp(email: string, otp: string): Promise<boolean> {
+        const cachecdOtp = await this.cacheStorage.getData(`otp/${email}`);
+        if (!cachecdOtp) {
+            throw new Error("OTP Expired or OTP not requested")
+        }
+        const otpVerified = otp === cachecdOtp;
+        if (otpVerified) {
+            await this.cacheStorage.deleteData(email);
+        }
+
+        return otpVerified;
+    }
+}
