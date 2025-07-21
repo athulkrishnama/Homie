69906a3ceb176124318fe1978ee987ca2cbdb2ad
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-21 15:45:36 +0530

    replaced user entity with createuser dto of createuserusecase return type

--- backend/src/domain/interfaces/useCase/auth/ICreateUser.ts
+++ backend/src/domain/interfaces/useCase/auth/ICreateUser.ts
@@ -1,5 +1,6 @@
 import { userEntity } from "../../../entities/user/userEntity";
+import { createUserDTO } from "../../DTOs/userDTOs/createUserDTO";
 
-export interface ICreateUserUseCase{
-    createUser(user:userEntity):Promise<userEntity>
+export interface ICreateUserUseCase {
+    createUser(user: userEntity): Promise<createUserDTO>
 }
