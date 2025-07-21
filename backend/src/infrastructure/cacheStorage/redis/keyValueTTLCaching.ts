77beb3936d0070d97a7943fc31af39da89cec1d7
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-21 11:33:04 +0530

    caching database implemented using redis

---
+++ backend/src/infrastructure/cacheStorage/redis/keyValueTTLCaching.ts
@@ -0,0 +1,42 @@
+import { createClient, RedisClientType } from "redis";
+import { IkeyValueTTLCaching } from "../../../domain/interfaces/respository/cacheStorage/IKeyValueTTLCaching";
+
+export class KeyValueTTLCaching implements IkeyValueTTLCaching {
+    private redisClient: RedisClientType;
+    constructor() {
+        this.redisClient = createClient({
+            url: process.env.REDIS_URL || 'redis://localhost:6379'
+        })
+
+        this.redisClient.on("error", (err) => console.log("Error occured on redis database", err));
+        this.redisClient.on("connect", () => console.log("Redis connected succesfully"));
+        this.redisClient.on("disconnect", () => console.log("Redis disconnected"))
+    }
+
+    async connect() {
+        if (!this.redisClient.isOpen) {
+            await this.redisClient.connect()
+        }
+    }
+
+    async setData(key: string, time: number, value: string): Promise<void> {
+        if (!this.redisClient.isOpen) {
+            await this.connect()
+        }
+        this.redisClient.setEx(key, time, value);
+    }
+
+    async getData(key: string): Promise<string | null> {
+        if (!this.redisClient.isOpen) {
+            await this.connect()
+        }
+        return await this.redisClient.get(key);
+    }
+
+    async deleteData(key: string): Promise<void> {
+        if (!this.redisClient.isOpen) {
+            await this.connect()
+        }
+        await this.redisClient.del(key)
+    }
+} 
