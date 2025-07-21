ea54846faf15030fc91c01a74aaded7243929f46
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-21 15:45:56 +0530

    add a default value to host status

--- backend/src/infrastructure/persistantStorage/mongodb/schema/userSchema.ts
+++ backend/src/infrastructure/persistantStorage/mongodb/schema/userSchema.ts
@@ -2,47 +2,48 @@ import { Schema } from "mongoose";
 import { userEntity } from "../../../../domain/entities/user/userEntity";
 
 export const userSchema = new Schema<userEntity>({
-    email:{
-        type:String,
-        required:[true, "Email address is required for user registration"],
-        unique:[true, "document with same email exists"]
+    email: {
+        type: String,
+        required: [true, "Email address is required for user registration"],
+        unique: [true, "document with same email exists"]
     },
-    fullname:{
-        type:String, 
-        required:function(){
+    fullname: {
+        type: String,
+        required: function () {
             return !this.isAdmin
         }
     },
-    password:{
-        type:String,
-        required:[true, "Password is required for user registration"]
+    password: {
+        type: String,
+        required: [true, "Password is required for user registration"]
     },
-    phone:{
-        type:String,
+    phone: {
+        type: String,
     },
-    profileImage:{
-        type:String
+    profileImage: {
+        type: String
     },
-    isBlocked:{
-        type:Boolean,
-        required:true,
-        default:false
+    isBlocked: {
+        type: Boolean,
+        required: true,
+        default: false
     },
-    hostStatus:{
-        type:String,
-        required:true,
-        enum:['disabled', 'active', 'blocked']
+    hostStatus: {
+        type: String,
+        required: true,
+        enum: ['disabled', 'active', 'blocked'],
+        default: 'disabled'
     },
-    isAdmin:{
-        type:Boolean,
-        required:true,
-        default:false
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
     },
-    createdAt:{
-        type:Date,
-        default:Date.now
+    createdAt: {
+        type: Date,
+        default: Date.now
     },
-    lastLogin:{
-        type:Date,
+    lastLogin: {
+        type: Date,
     }
 })
