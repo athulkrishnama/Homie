85ca45a85c01f665a5d7ac01a2108506656ec7ad
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-21 11:33:29 +0530

    user model and user schema created for mongodb

---
+++ backend/src/infrastructure/persistantStorage/mongodb/schema/userSchema.ts
@@ -0,0 +1,48 @@
+import { Schema } from "mongoose";
+import { userEntity } from "../../../../domain/entities/user/userEntity";
+
+export const userSchema = new Schema<userEntity>({
+    email:{
+        type:String,
+        required:[true, "Email address is required for user registration"],
+        unique:[true, "document with same email exists"]
+    },
+    fullname:{
+        type:String, 
+        required:function(){
+            return !this.isAdmin
+        }
+    },
+    password:{
+        type:String,
+        required:[true, "Password is required for user registration"]
+    },
+    phone:{
+        type:String,
+    },
+    profileImage:{
+        type:String
+    },
+    isBlocked:{
+        type:Boolean,
+        required:true,
+        default:false
+    },
+    hostStatus:{
+        type:String,
+        required:true,
+        enum:['disabled', 'active', 'blocked']
+    },
+    isAdmin:{
+        type:Boolean,
+        required:true,
+        default:false
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    lastLogin:{
+        type:Date,
+    }
+})
