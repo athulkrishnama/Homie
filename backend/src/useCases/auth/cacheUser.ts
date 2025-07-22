31e2956f3268885f657c29fb9ca0edb6de0db966
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-22 11:11:04 +0530

    user caching implemented

---
+++ backend/src/useCases/auth/cacheUser.ts
@@ -0,0 +1,13 @@
+import { loginUserDTO } from "../../domain/interfaces/DTOs/userDTOs/loginUserDTO";
+import { IkeyValueTTLCaching } from "../../domain/interfaces/respository/cacheStorage/IKeyValueTTLCaching";
+import { ICacheUserUseCase } from "../../domain/interfaces/useCase/auth/ICacheUser";
+
+export class CacheUserUseCase implements ICacheUserUseCase {
+    constructor(private cacheDatabase: IkeyValueTTLCaching) {
+
+    }
+
+    cacheUser(user: loginUserDTO): void {
+        this.cacheDatabase.setData(`user/${user._id}`, 15 * 60, JSON.stringify(user));
+    }
+}
