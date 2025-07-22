67deb34bcc81df5f255d3cee51e708adc5acfcfa
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-22 11:13:40 +0530

    add login user controller and use cases for user login functionality

--- backend/src/infrastructure/DI/auth.ts
+++ backend/src/infrastructure/DI/auth.ts
@@ -1,13 +1,18 @@
+import { LoginUserController } from "../../interfaceAdapters/controllers/auth/loginUser";
 import { RegisterUserController } from "../../interfaceAdapters/controllers/auth/registerUser";
 import { sendOtpController } from "../../interfaceAdapters/controllers/auth/sendOtp";
+import { CacheUserUseCase } from "../../useCases/auth/cacheUser";
 import { CreateUserUseCase } from "../../useCases/auth/createUser";
 import { sendOtpUseCase } from "../../useCases/auth/sendOtp";
+import {  TokenCreationUseCase } from "../../useCases/auth/TokenCreation";
+import {  UserLoginUseCase } from "../../useCases/auth/userlogin";
 import { VerifyOtpUseCase } from "../../useCases/auth/verifyOtp";
 import { KeyValueTTLCaching } from "../cacheStorage/redis/keyValueTTLCaching";
 import { userRepository } from "../persistantStorage/mongodb/repositories/userRepository";
 import { OtpEmailContentGenerator } from "../services/emailContentGenerator/otpEmailContentGenerator";
 import { EmailService } from "../services/emailService";
 import { HashService } from "../services/HashService";
+import { JWTService } from "../services/JWTService";
 import { OtpService } from "../services/otpService";
 
 // Create User Send Otp Controller
@@ -26,3 +31,13 @@ const injectedCreateUserUseCase = new CreateUserUseCase(userPersistance, hashSer
 const injectedVerifyOtpUseCase = new VerifyOtpUseCase(cacheStorage)
 
 export const injectedCreateUserController = new RegisterUserController(injectedVerifyOtpUseCase, injectedCreateUserUseCase)
+
+// Login Controller
+const jwtService = new JWTService()
+const tokenCreationUseCase = new TokenCreationUseCase(jwtService);
+
+const userLoginUseCase = new UserLoginUseCase(userPersistance, hashService)
+
+const cacheUserUseCase = new CacheUserUseCase(cacheStorage)
+
+export const injectedLoginUserController = new LoginUserController(userLoginUseCase, tokenCreationUseCase, cacheUserUseCase)
