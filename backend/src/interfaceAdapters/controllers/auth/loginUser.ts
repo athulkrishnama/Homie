1a241673e60266599c0301f4cddf8f7d7c78920f
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-22 12:23:40 +0530

    user login refresh token bug fixed

--- backend/src/interfaceAdapters/controllers/auth/loginUser.ts
+++ backend/src/interfaceAdapters/controllers/auth/loginUser.ts
@@ -5,7 +5,7 @@ import { HTTPStatus } from "../../../domain/entities/httpStatus";
 import { ICacheUserUseCase } from "../../../domain/interfaces/useCase/auth/ICacheUser";
 
 export class LoginUserController {
-    constructor(private loginUseCase: IUserLogin, private tokenCreation: ITokenCreationUseCase, private cacheUserUseCase:ICacheUserUseCase) {
+    constructor(private loginUseCase: IUserLogin, private tokenCreation: ITokenCreationUseCase, private cacheUserUseCase: ICacheUserUseCase) {
 
     }
 
@@ -16,12 +16,12 @@ export class LoginUserController {
             const user = await this.loginUseCase.userLogin(email, password);
 
             const token = this.tokenCreation.createAccessTokenAndRefreshToken({ userId: user._id.toString(), role: "user" });
-            res.cookie('RefreshToken', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+            res.cookie('RefreshToken', token.refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
 
             // setting data in caching database
             this.cacheUserUseCase.cacheUser(user);
 
-            res.status(HTTPStatus.OK).json({"message":"User Login Successful", user, accessToken:token.accessToken});
+            res.status(HTTPStatus.OK).json({ "message": "User Login Successful", user, accessToken: token.accessToken });
 
         } catch (error) {
             res.status(HTTPStatus.BAD_REQUEST).json({ message: "Error while login", error: (error instanceof Error ? error.message : "Error while validation user") })
