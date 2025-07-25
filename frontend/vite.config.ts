6e2b1b6346f8a0baf18a902c2e257651aaaf85d8
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-25 12:44:23 +0530

    frontend initialised with tailwind css , shadcn ui and vite pwa plugin

---
+++ frontend/vite.config.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: "Homie",
+      short_name: "Homie",
+      description: "Homie -Hotel Booking Application",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          "src": "pwa-64x64.png",
+          "sizes": "64x64",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "maskable-icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ]
+    }
+  })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+})
