33a1b05ff1455aa0b4a058f6fdc11e03ad75f5c1
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-26 11:44:37 +0530

    tanstack router plugin added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,41 +3,49 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({
-    registerType: 'autoUpdate',
-    manifest: {
-      name: "Homie",
-      short_name: "Homie",
-      description: "Homie -Hotel Booking Application",
-      theme_color: "#ffffff",
-      icons: [
-        {
-          "src": "pwa-64x64.png",
-          "sizes": "64x64",
-          "type": "image/png"
-        },
-        {
-          "src": "pwa-192x192.png",
-          "sizes": "192x192",
-          "type": "image/png"
-        },
-        {
-          "src": "pwa-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png"
-        },
-        {
-          "src": "maskable-icon-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png",
-          "purpose": "maskable"
-        }
-      ]
-    }
-  })],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true
+    }),
+    tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Homie",
+        short_name: "Homie",
+        description: "Homie -Hotel Booking Application",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
+    })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
