287e14585d4b6e994fa861f5b186a924745e983f
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-26 11:49:24 +0530

    i18n library for internationalisation added

---
+++ frontend/src/utils/i18n/index.ts
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import mlLang from './locales/ml/ml.json'
+import enLang from './locales/en/en.json'
+const resources = {
+    en:{
+        translation:enLang
+    },
+
+    ml:{
+        translation:mlLang
+    }
+}
+
+i18n.use(initReactI18next).init({
+    resources,
+    fallbackLng:'ml',
+    
+})
