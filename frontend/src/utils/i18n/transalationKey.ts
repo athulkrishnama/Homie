f6c02ce1923cc8c119fc7e2051c28e3fd5c75615
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-27 19:52:24 +0530

    new transalations added

--- frontend/src/utils/i18n/transalationKey.ts
+++ frontend/src/utils/i18n/transalationKey.ts
@@ -4,7 +4,29 @@ export default {
     },
     button: {
         save: "button.save",
-        submit:"button.submit",
-        submiting:"button.submiting"
+        submit: "button.submit",
+        submiting: "button.submiting"
+    },
+    userlogin: {
+        form: {
+            labels: {
+                email: "userlogin.form.labels.email",
+                password: "userlogin.form.labels.password",
+                register: "userlogin.form.labels.register",
+                login: "userlogin.form.labels.login"
+            },
+            validations: {
+                email: {
+                    required: "userlogin.form.validations.email.required",
+                    invalid: "userlogin.form.validations.email.invalid"
+                },
+                password: {
+                    required: 'userlogin.form.validations.password.required',
+                    min: 'userlogin.form.validations.password.min',
+                    max: 'userlogin.form.validations.password.max'
+
+                }
+            }
+        }
     }
 }
