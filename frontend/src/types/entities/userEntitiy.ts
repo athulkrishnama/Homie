a008ee2dd455427b08eba7203e1b7d712cf5aa64
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-28 09:52:19 +0530

    make createdAt, lastLogin, and profileImage optional in UserEntity interface

--- frontend/src/types/entities/userEntitiy.ts
+++ frontend/src/types/entities/userEntitiy.ts
@@ -2,12 +2,12 @@ export interface UserEntity {
     _id?: string,
     email: string,
     password: string,
-    createdAt: Date,
-    lastLogin: Date,
+    createdAt?: Date,
+    lastLogin?: Date,
     isAdmin: boolean,
     fullname: string,
     phone: string,
     isBlocked: boolean,
-    profileImage: string,
+    profileImage?: string,
     hostStatus: 'disabled' | 'active' | 'blocked'
 }
