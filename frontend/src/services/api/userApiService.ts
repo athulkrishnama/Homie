3b07b433f2a20562f61ceb75d26e334a7e2f7d41
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-29 04:14:41 +0530

    user singup routed added

--- frontend/src/services/api/userApiService.ts
+++ frontend/src/services/api/userApiService.ts
@@ -1,8 +1,7 @@
 import { axiosInstance } from "@/axios/instance";
-import type { userLoginDTO } from "@/types/api/userLogin";
 import { AxiosError } from "axios";
 
-export const userLogin = async (data: userLoginDTO) => {
+export const userLogin = async <T>(data: T) => {
     try {
         const response = await axiosInstance.post('/auth/login', data);
         return response.data
@@ -12,5 +11,45 @@ export const userLogin = async (data: userLoginDTO) => {
             throw new Error(err?.response?.data?.error ?? "Error while loggin in")
         }
         throw new Error("Error while login")
+    }
+}
+
+export const userSignupSendOtp = async <T>(data: T) => {
+    try {
+        const response = await axiosInstance.post('/auth/signup', data);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        if (error instanceof AxiosError) {
+            throw new Error(error?.response?.data?.error ?? "Error while signup");
+        }
+    }
+    throw new Error("Error while sending OTP");
+}
+
+export const userSignupVerify = async <T>(data: T) => {
+    try {
+        const response = await axiosInstance.post('/auth/verify', data);
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        if (err instanceof AxiosError) {
+            throw new Error(err.response?.data.error ?? "Error while verifying");
+        }
+        throw new Error("Error while Verifying")
+    }
+}
+
+
+export const userSignupResendOtp = async<T>(data: T) => {
+    try {
+        const response = await axiosInstance.post('/auth/resendOtp', data);
+        return response.data
+    } catch (error) {
+        console.log(error);
+        if (error instanceof AxiosError) {
+            throw new Error(error.response?.data.error ?? "Error while resending OTP");
+        }
+        throw new Error("Error while resending OTP")
     }
 }
