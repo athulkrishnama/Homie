8ee31a2deef455cd9c165018e2f41c997194bf11
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-29 04:14:54 +0530

    user signup mutations added

--- frontend/src/hooks/userApiHook.tsx
+++ frontend/src/hooks/userApiHook.tsx
@@ -1,10 +1,31 @@
-import { userLogin } from "@/services/api/userApiService"
-import type { userLoginDTO } from "@/types/api/userLogin"
-import type { UserLoginResponseDTO } from "@/types/api/userLoginResponse"
-import { useMutation } from "@tanstack/react-query"
+import { userLogin, userSignupResendOtp, userSignupSendOtp as userSignupSendOtpApi, userSignupVerify } from "@/services/api/userApiService"
+import type { userLoginDTO } from "@/types/api/requestTypes/userLogin"
+import { type IBaseResponse } from "@/types/api/responseTypes/baseResponse"
+import type { UserLoginResponse } from "@/types/api/responseTypes/userLoginResponse"
+import { useMutation } from "@tanstack/react-query";
+import type { userSignup } from '@/types/api/requestTypes/userSignup'
+import type { userSignupSendOtp } from "@/types/api/requestTypes/userSignupSendOtp";
 
 export const useUserLoginMutation = () => {
-    return useMutation<UserLoginResponseDTO, Error, userLoginDTO>({
-        mutationFn: ({ email, password }: userLoginDTO) => userLogin({ email, password })
+    return useMutation<UserLoginResponse, Error, userLoginDTO>({
+        mutationFn: ({ email, password }: userLoginDTO) => userLogin<userLoginDTO>({ email, password })
+    })
+}
+
+export const useUserSignupSendOtpMutation = () => {
+    return useMutation<IBaseResponse, Error, userSignupSendOtp>({
+        mutationFn: ({ email }: userSignupSendOtp) => userSignupSendOtpApi<userSignupSendOtp>({ email })
+    })
+}
+
+export const useUserSignupVerifyMutation = ()=>{
+    return useMutation<IBaseResponse, Error, userSignup>({
+        mutationFn:(signupData:userSignup)=> userSignupVerify<userSignup>(signupData)
+    })
+}
+
+export const useUserSignupResendOtpMutation = ()=>{
+    return useMutation<IBaseResponse, Error, userSignupSendOtp>({
+        mutationFn: ({email}:userSignupSendOtp)=> userSignupResendOtp<userSignupSendOtp>({email})
     })
 }
