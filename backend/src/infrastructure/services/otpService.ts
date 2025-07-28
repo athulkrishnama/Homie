ce3dd43e515264ed8c566bbb0c68436d7274550a
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-29 04:15:54 +0530

    otp digit changed form 4 to 6

--- backend/src/infrastructure/services/otpService.ts
+++ backend/src/infrastructure/services/otpService.ts
@@ -2,6 +2,6 @@ import { IOtpService } from "../../domain/interfaces/service/IOtp";
 
 export class OtpService implements IOtpService{
     generateOtp(): string {
-        return Math.floor((Math.random() * 9000) + 1000).toString()
+        return Math.floor((Math.random() * 900000) + 100000).toString()
     } 
 }
