baaa44a7660d0294102ca31bb66829dac2cc787c
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-29 08:29:58 +0530

    whitelisted user slice for persistance

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -8,7 +8,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig= {
     key: 'root',
     storage,
-    whitelist:['lang']    
+    whitelist:['lang', 'user']    
 }
 
 const rootReducer = combineReducers({
