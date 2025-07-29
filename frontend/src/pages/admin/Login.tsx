f3fc8829b2c7442a0e67fd3074376b48491d433a
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-29 11:20:17 +0530

    admin login page created

---
+++ frontend/src/pages/admin/Login.tsx
@@ -0,0 +1,18 @@
+import AdminLoginForm from '@/components/admin/login/AdminLoginForm'
+import transalationKey from '@/utils/i18n/transalationKey'
+import { useTranslation } from 'react-i18next'
+import { motion } from 'motion/react'
+
+function Login() {
+    const { t } = useTranslation()
+    return (
+        <div className='h-screen w-screen flex items-center justify-center bg-zinc-200'>
+            <div className='w-1/3 h-1/2 bg-white rounded-2xl shadow-2xl flex flex-col items-center justify-center'>
+                <motion.h2 initial={{ scale: 0.5 }} animate={{ scale: 1 }} transition={{ duration: 0.3, type: 'spring', damping: 8 }} className='font-bold text-5xl text-center mb-5'>{t(transalationKey.adminLogin.form.lables.adminLogin)}</motion.h2>
+                <AdminLoginForm />
+            </div>
+        </div>
+    )
+}
+
+export default Login
