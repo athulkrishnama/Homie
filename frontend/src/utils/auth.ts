baa66611252968dd0de6933f8d2c72ab0c720090
athulkrishnama <athulkrishnama24@gmail.com>
2025-07-29 11:16:35 +0530

    auth logic changed

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -1,5 +1,9 @@
 import { store } from "@/store/store"
 
 export const isAuthenticated = (): boolean => {
-    return store.getState().user.data !== null && store.getState().user.data.isAdmin === false;
+    return store.getState().user.isLogin && store.getState().user.data.isAdmin === false;
+}
+
+export const isAdmin = (): boolean => {
+    return store.getState().user.isLogin && store.getState().user.data.isAdmin === true;
 }
