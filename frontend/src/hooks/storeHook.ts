2d2090fe92c5cd978b749190eae30d68b8bf74c4
athulkrishnama <athulkrishnama24@gmail.com>
2025-08-01 15:24:57 +0530

    type bug fixed

--- frontend/src/hooks/storeHook.ts
+++ frontend/src/hooks/storeHook.ts
@@ -1,5 +1,6 @@
-import type { AppDispatch, AppStore } from '@/store/store';
-import {useDispatch, useSelector} from 'react-redux';
+import type { AppDispatch, AppStore, RootState } from '@/store/store';
+import { useDispatch, useSelector, useStore } from 'react-redux';
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<AppStore>();
+export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppStore = useStore.withTypes<AppStore>();
