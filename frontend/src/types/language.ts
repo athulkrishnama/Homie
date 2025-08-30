3e30f57491d5783d4a557230fbd42876338960c6
athulkrishnama <athulkrishnama24@gmail.com>
2025-08-30 16:12:03 +0530

    tamil and hindi languages added

--- frontend/src/types/language.ts
+++ frontend/src/types/language.ts
@@ -1,2 +1,2 @@
-export const Languages = ['en', 'ml'] as const;
+export const Languages = ['en', 'ml', 'hi', 'ta'] as const;
 export type Langtype = typeof Languages[number];
--- frontend/src/utils/i18n/index.ts
+++ frontend/src/utils/i18n/index.ts
@@ -1,19 +1,28 @@
 import i18n from 'i18next';
-import {initReactI18next} from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
 import mlLang from './locales/ml/ml.json'
 import enLang from './locales/en/en.json'
+import taLang from './locales/ta/ta.json'
+import hiLang from './locales/hi/hi.json'
+
 const resources = {
-    en:{
-        translation:enLang
+    en: {
+        translation: enLang
     },
 
-    ml:{
-        translation:mlLang
+    ml: {
+        translation: mlLang
+    },
+    ta: {
+        translation: taLang
+    },
+    hi: {
+        translation: hiLang
     }
 }
 
 i18n.use(initReactI18next).init({
     resources,
-    fallbackLng:'en',
-    
+    fallbackLng: 'en',
+
 })
