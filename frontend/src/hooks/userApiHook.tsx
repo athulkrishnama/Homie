7e4d1f7a2b5bccd25c59af2b5bd66b8218431de4
athulkrishnama <athulkrishnama24@gmail.com>
2025-08-30 15:53:34 +0530

    hook for forgetpassword and verify forgetpassword added

--- frontend/src/hooks/userApiHook.tsx
+++ frontend/src/hooks/userApiHook.tsx
@@ -1,10 +1,12 @@
-import { userLogin, userSignupResendOtp, userSignupSendOtp as userSignupSendOtpApi, userSignupVerify } from "@/services/api/userApiService"
+import { userForgetPasswordRequestOtp, userForgetPasswordVerifyOtp, userLogin, userSignupResendOtp, userSignupSendOtp as userSignupSendOtpApi, userSignupVerify } from "@/services/api/userApiService"
 import type { userLoginRequestType } from "@/types/api/requestTypes/userLogin"
 import { type IBaseResponse } from "@/types/api/responseTypes/baseResponse"
 import type { UserLoginResponse } from "@/types/api/responseTypes/userLoginResponse"
 import { useMutation } from "@tanstack/react-query";
 import type { userSignup } from '@/types/api/requestTypes/userSignup'
 import type { userSignupSendOtp } from "@/types/api/requestTypes/userSignupSendOtp";
+import type { userForgetPasswordSendOtpType } from "@/types/api/requestDTO/userForgetPasswordSendOtp";
+import type { userForgetPasswordVerifyOtpType } from "@/types/api/requestDTO/userForgetPasswordVerify"
 
 export const useUserLoginMutation = () => {
     return useMutation<UserLoginResponse, Error, userLoginRequestType>({
@@ -18,14 +20,26 @@ export const useUserSignupSendOtpMutation = () => {
     })
 }
 
-export const useUserSignupVerifyMutation = ()=>{
+export const useUserSignupVerifyMutation = () => {
     return useMutation<IBaseResponse, Error, userSignup>({
-        mutationFn:(signupData:userSignup)=> userSignupVerify<userSignup>(signupData)
+        mutationFn: (signupData: userSignup) => userSignupVerify<userSignup>(signupData)
     })
 }
 
-export const useUserSignupResendOtpMutation = ()=>{
+export const useUserSignupResendOtpMutation = () => {
     return useMutation<IBaseResponse, Error, userSignupSendOtp>({
-        mutationFn: ({email}:userSignupSendOtp)=> userSignupResendOtp<userSignupSendOtp>({email})
+        mutationFn: ({ email }: userSignupSendOtp) => userSignupResendOtp<userSignupSendOtp>({ email })
+    })
+}
+
+export const useUserForgetPasswordRequestOtp = () => {
+    return useMutation<IBaseResponse, Error, userForgetPasswordSendOtpType>({
+        mutationFn: ({ email }: userForgetPasswordSendOtpType) => userForgetPasswordRequestOtp<userForgetPasswordSendOtpType>({ email })
+    })
+}
+
+export const useUserForgetPasswordVerifyOtp = () => {
+    return useMutation({
+        mutationFn: (data: userForgetPasswordVerifyOtpType) => userForgetPasswordVerifyOtp<userForgetPasswordVerifyOtpType>(data)
     })
 }
