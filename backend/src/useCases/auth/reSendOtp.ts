36d9638d1a545b13ab23a52d3afe9ef9166030db
athulkrishnama <athulkrishnama24@gmail.com>
2025-09-02 11:33:22 +0530

    refactor: improve resend OTP use case by adding user persistence check

--- backend/src/useCases/auth/reSendOtp.ts
+++ backend/src/useCases/auth/reSendOtp.ts
@@ -1,3 +1,4 @@
+import { IUserPersistance } from "../../domain/interfaces/respository/persistentStorage/IUserPersistance";
 import { IkeyValueTTLCaching } from "../../domain/interfaces/service/cacheStorage/IKeyValueTTLCaching";
 import { IOtpEmailContentGenerator } from "../../domain/interfaces/service/emailContentGenerators/IOtpEmailContentGenerator";
 import { IOtpEmailTemplate } from "../../domain/interfaces/service/emailTemplate/IOtpEmailTemplate";
@@ -6,17 +7,28 @@ import { IOtpService } from "../../domain/interfaces/service/IOtp";
 import { IResendOtpUseCase } from "../../domain/interfaces/useCase/auth/IResendOtp";
 
 export class ResendOtpUseCase implements IResendOtpUseCase {
-    constructor(private otpService: IOtpService, private otpmailContentGenerator: IOtpEmailContentGenerator, private emailSerivce: IEmailService, private cacheDatabase: IkeyValueTTLCaching) {
+    constructor(private otpService: IOtpService,
+        private otpmailContentGenerator: IOtpEmailContentGenerator,
+        private emailSerivce: IEmailService,
+        private cacheDatabase: IkeyValueTTLCaching,
+        private userPersistance: IUserPersistance) {
 
     }
 
-    async resendOtp(email:string): Promise<void> {
+    async resendOtp(email: string): Promise<void> {
+
+        const user = await this.userPersistance.findByEmail(email);
+
+        if (!user) {
+            throw new Error("Email is not existing")
+        }
+
         const OTP = this.otpService.generateOtp();
 
-        const emailTemplate:IOtpEmailTemplate = {
-            receiverMail:email,
-            otp:OTP,
-            subject:"Resend OTP",
+        const emailTemplate: IOtpEmailTemplate = {
+            receiverMail: email,
+            otp: OTP,
+            subject: "Resend OTP",
         }
 
         const content = this.otpmailContentGenerator.generateTemplate(OTP);
