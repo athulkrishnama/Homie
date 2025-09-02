bf0c4b97770e2f67e314676736381263d9c07294
athulkrishnama <athulkrishnama24@gmail.com>
2025-09-02 11:32:36 +0530

    custom image added in forget password page

--- frontend/src/components/shared/forgotPassword/ForgotPassword.tsx
+++ frontend/src/components/shared/forgotPassword/ForgotPassword.tsx
@@ -20,9 +20,10 @@ export interface forgetPasswordProps {
     onOtpRequest(email: string): Promise<boolean>
     onOtpSubmit(email: string, otp: string, password: string): Promise<void>
     onResendOtp(email: string): void,
+    image?: string
 }
 
-function ForgotPassword({ heading, onOtpRequest, onOtpSubmit, onResendOtp }: forgetPasswordProps) {
+function ForgotPassword({ heading, onOtpRequest, onOtpSubmit, onResendOtp, image }: forgetPasswordProps) {
     const [isOpen, setIsOpen] = useState(false)
 
     const router = useRouter()
@@ -49,7 +50,7 @@ function ForgotPassword({ heading, onOtpRequest, onOtpSubmit, onResendOtp }: for
             <div className='w-screen h-screen bg-zinc-900 flex justify-center items-center'>
                 <div className='w-1/2 h-1/2 flex  bg-neutral-100 rounded-4xl shadow-[3px_-3px_40px_3px_#aaaaaa]'>
                     <div className='flex-1/2'>
-                        <img src={Logo} alt="" />
+                        <img src={image || Logo} alt="" />
                     </div>
                     <div className='flex-1/2 px-5 py-10 '>
                         <h1 className='text-center text-5xl font-bold'>{heading}</h1>
