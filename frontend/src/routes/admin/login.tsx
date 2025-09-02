fd957e234def6ab46e3f9852f4c81b7de1cc4619
athulkrishnama <athulkrishnama24@gmail.com>
2025-09-02 11:30:35 +0530

    added checking for is admin is already logged in added

--- frontend/src/routes/admin/login.tsx
+++ frontend/src/routes/admin/login.tsx
@@ -1,7 +1,13 @@
 import Login from '@/pages/admin/Login'
-import { createFileRoute } from '@tanstack/react-router'
+import { isAdmin } from '@/utils/auth'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/admin/login')({
+  beforeLoad: () => {
+    if (isAdmin()) {
+      throw redirect({ to: '/admin', replace: true })
+    }
+  },
   component: Login,
 })
 
